461e43cbc4a73d5729fd6ce96308c83d1d53c658
nazilataheri <50323320+nazilataheri@users.noreply.github.com>
2020-10-09 10:17:23 +1100

    Add files via upload

---
+++ src/containers/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState('');
+
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
+  }
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {setRobots(users)});
+  });
+  if(robots.length === 0){
+    return <h1>Loading...</h1>
+  }else{
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots}/>
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
