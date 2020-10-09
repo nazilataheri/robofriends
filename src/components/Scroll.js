8af3944326ef5339971e0ce567fcdfe4f8ed004e
nazilataheri@gmail.com <nazila@SanickBook-Air.local>
2020-10-09 11:31:00 +1100

    add github pages and remove a semicolon from Scroll component

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -4,7 +4,7 @@ const Scroll = (props) => {
 
   return (
     <div style={{overflowY: 'scroll', border: '5px solid black', height: '700px'}}>
-      {props.children};
+      {props.children}
     </div>
   )
 };
