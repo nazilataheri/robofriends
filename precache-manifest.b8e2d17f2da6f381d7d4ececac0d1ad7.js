989046e1b83f9f4a642cd2df036cc013a2ba085e
nazilataheri@gmail.com <nazila@SanickBook-Air.local>
2020-10-09 11:27:24 +1100

    Updates

---
+++ precache-manifest.b8e2d17f2da6f381d7d4ececac0d1ad7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c936e51f46fd0558dc5a8f002ae31a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4866b8f79590b3c8c3ae",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d9ebfa53b28742577dbc",
+    "url": "/robofriends/static/css/main.d5c89190.chunk.css"
+  },
+  {
+    "revision": "4866b8f79590b3c8c3ae",
+    "url": "/robofriends/static/js/2.9a292bed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.9a292bed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ebfa53b28742577dbc",
+    "url": "/robofriends/static/js/main.07453022.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
